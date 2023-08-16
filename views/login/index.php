e4b3879c2ec2384fa23f55b8aaaf50a74bbe1e0a
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-08-16 08:17:29 -0600

    cambio en el layout

---
+++ views/login/index.php
@@ -0,0 +1,15 @@
+
+</head>
+<body>
+    <h2>Iniciar Sesión</h2>
+    <form>
+        <label for="usuario">Usuario:</label>
+        <input type="text" id="usuario" name="usuario" required>
+        <br>
+        <label for="contrasena">Contraseña:</label>
+        <input type="password" id="contrasena" name="contrasena" required>
+        <br>
+        <input type="submit" value="Iniciar Sesión">
+    </form>
+</body>
+</html>
