736893c5b437a8cb07c6b63913a2534da5ab702f
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-08-17 21:44:52 -0600

    agregar direccion al index

--- src/js/menu/index.js
+++ src/js/menu/index.js
@@ -1,16 +1,29 @@
-const botonCerrarSesion = document.getElementById('CerrarSesion');
+const formLogin = document.querySelector('form');
 
-const cerrarSesion = () => {
-    // Realiza aquí la lógica para cerrar la sesión (eliminar cookies, variables de sesión, etc.)
-    window.location.href = '/login/'; // Redirigir a la página de inicio de sesión
-};
+const closeSessionRequest = async e => {
+    e.preventDefault();
 
-if (botonCerrarSesion) {
-    botonCerrarSesion.addEventListener('click', cerrarSesion);
+
+    try {
+        const url = "/login/API/closeSession"; 
+     
+        const headers = new Headers();
+        headers.append("X-Requested-With", "fetch");
+
+        const config = {
+            method: 'GET',
+            headers
+        };
+        
+        const respuesta = await fetch(url,config);
+        
+        
+        window.location.href = '/login/';
+
+      
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-// Evitar que el usuario regrese a la página de inicio de sesion
-window.history.pushState(null, null, window.location.href);
-window.onpopstate = function(event) {
-    window.history.go(1);
-};
+formLogin.addEventListener('submit', closeSessionRequest);
